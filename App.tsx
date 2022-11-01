d6152b03ade62e9c76a9cff7b9d5a9c23949451d
Tomas Franco <tomasfranco@Tomas-MacBook-Pro.local>
2022-11-01 00:43:07 -0300

    Instalando e configurando - NativeBase

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NativeBaseProvider } from "native-base";
+
 export default function App() {
   return (
+    <NativeBaseProvider>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Vamos testar para ver se esta dando certo
+        
+      </Text>
       <StatusBar style="auto" />
     </View>
+    </NativeBaseProvider>
   );
 }
 
