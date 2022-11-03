7a3b3265743e8938f1dbf0965b7887ab3237d3e3
Tomas Franco <tomasfranco@Tomas-MacBook-Pro.local>
2022-11-03 17:49:54 -0300

    Instalando OAuth com autenticacao via login Google

--- App.tsx
+++ App.tsx
@@ -6,18 +6,21 @@ import { SignIn } from "./src/screens/SigIn";
 import { Loading } from "./src/components/Loading";
 
 import { THEME } from "./src/styles/theme";
+import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold});
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <AuthContextProvider>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       {fontsLoaded ? <SignIn /> : <Loading />}    
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, ReactNode } from "react";
+
+interface UserProps {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+  signIn: () => Promise<void>;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthProviderProps) {
+
+    async function signIn() {
+      console.log('Vamos logar!')
+    }
+
+    return (
+      <AuthContext.Provider value={{
+        signIn,
+        user: {
+          name: 'Tomás',
+          avatarUrl: 'https://github.com/tomasfranco.png'
+        }
+      }}>
+        {children}
+      </AuthContext.Provider>
+    )
+}
